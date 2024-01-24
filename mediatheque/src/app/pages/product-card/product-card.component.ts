f8b2927a921effec38edda1d67a4add8c4e37556
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2024-01-24 16:31:14 +0100

    tp4 angular

---
+++ mediatheque/src/app/pages/product-card/product-card.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [MatCardModule, CommonModule,RouterModule],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css'
+})
+export class ProductCardComponent {
+
+  @Input() 
+  product!: any;
+
+}
